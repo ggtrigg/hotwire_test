6d82cadfb856ec6676e90e79e1f8330d4b8a4b5c
Glenn Trigg <ggtrigg@gmail.com>
2021-09-30 21:10:47 +1000

    One more piece for deploying in a subdirectory.

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -4,6 +4,9 @@ class Shared::LayoutHead < BaseComponent
   def render
     head do
       utf8_charset
+      if LuckyEnv.production?
+        tag "base", href: "/hotwire_test/"
+      end
       title "My App - #{@page_title}"
       css_link "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css", defer: "true",
         data_turbolinks_track: "reload", integrity: "sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6",
